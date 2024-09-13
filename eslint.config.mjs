293d7ead46df396c5df97333b61d1b41bba0bafe
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-13 17:35:58 +0530

    add test3 husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,21 +5,21 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
-  extends: [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-    eslintConfigPrettier,
-  ],
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
+        eslintConfigPrettier,
+    ],
+    languageOptions: {
+        parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
     },
-  },
 
-  files: ['**/*.ts'],
-  rules: {
-    // "no-console": "warn"
-  },
+    files: ['**/*.ts'],
+    rules: {
+        "no-console": "warn"
+    },
 });
