aca74ba8fb000145afd3f27a54b74b87aedb9dba
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-14 16:11:24 +0530

    add colorize

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 import logger from './config/logger';
 import Config from './config/config';
 
 const app = express();
+const morganStream = {
+  write: (message: string) => {
+    logger.http(message.trim());
+  },
+};
+
+app.use(morgan('tiny', { stream: morganStream }));
+
+app.get('/test-route', (_req: Request, res: Response) => {
+  res.json({ success: true });
+});
 
 const startServer = () => {
   app.listen(Config.PORT, () => {
-    logger.error(`Connected to server ${Config.PORT}`);
+    logger.info(`Server is running on port ${Config.PORT}`);
   });
 };
 
