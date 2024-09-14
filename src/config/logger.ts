99224ccb9aaeb64f5c34e1020b69a847dfab106b
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-14 16:11:24 +0530

    add colorize

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,18 +1,37 @@
 import winston from 'winston';
 
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  debug: 'blue',
+};
+
+winston.addColors(colors);
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
+  level: 'http',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
   defaultMeta: { service: 'auth-service' },
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
+        winston.format.colorize(),
         winston.format.timestamp(),
-        winston.format.json(),
+        winston.format.simple(),
       ),
     }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({
+      filename: 'logs/http.log',
+      level: 'http',
+      format: winston.format((info) => {
+        return info.level === 'http' ? info : false;
+      })(),
+    }),
   ],
 });
 
--- todo.md
+++ todo.md
@@ -7,8 +7,8 @@
 [*] logger [winston] {cm:2024-09-13}
 [*] express js {cm:2024-09-13}
 [*] dot env {cm:2024-09-13}
+[*] colorfull terminal {cm:2024-09-14}
+[*] first code {cm:2024-09-14}
 [*] database
-[*] first code
-[*] colorfull terminal
 [*] commitlinting
 [*] db-migration
