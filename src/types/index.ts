1ebe7f40c511266bbb7ca8a34af9590e61253cdc
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-16 12:59:27 +0530

    add User model

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+import { Types } from 'mongoose';
+export type UserRole = 'USER' | 'ADMIN' | 'MANAGER';
+export type UserStatus = 'ACTIVE' | 'INACTIVE' | 'BANNED';
+
+export interface User {
+  email: string;
+  name: string;
+  password: string;
+  devices: number;
+  channelName: string;
+  lastLogin: Date;
+  coverImgUrl: string;
+  socialProfileUrl: string[];
+  role: UserRole;
+  status: UserStatus;
+  profilePicture: string;
+  subscribers: number;
+  subscribedTo: Types.ObjectId[];
+  likedVideos: Types.ObjectId[];
+  dislikedVideos: Types.ObjectId[];
+  watchHistory: Types.ObjectId[];
+  playlists: {
+    name: string;
+    videos: Types.ObjectId[];
+  }[];
+  notifications: {
+    message: string;
+    isRead: boolean;
+    createdAt: Date;
+  }[];
+}
